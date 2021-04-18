a813ff022212b99324aa7c8d5a19eccd668eeaf3
Randy Hommel <randy@hommel.name>
2021-04-18 09:02:40 -0700

    Updating apache config for allowing htaccess, and updating aws cli to v2

--- opt/install/update_os.sh
+++ opt/install/update_os.sh
@@ -2,8 +2,13 @@
 
 ### Update OS
 apt-get update -y && apt-get upgrade -y
-apt-get install -y vim zlib1g-dev python libpng-dev libjpeg-dev libzip-dev
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-python /tmp/get-pip.py
-pip install --upgrade pip && pip install awscli
+apt-get install -y vim zlib1g-dev libpng-dev libjpeg-dev libzip-dev unzip
+
+### Install AWS CLI
+
+cd /opt
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
 apt-get autoremove -y
